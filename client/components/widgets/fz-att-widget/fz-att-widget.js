7773ebaed4ce1fe6e3ce5e6a6149a11be6f66bd0
PS <monsterxxx@yandex.ru>
2016-03-24 14:08:07 +0700

    debuggin

--- client/components/widgets/fz-att-widget/fz-att-widget.js
+++ client/components/widgets/fz-att-widget/fz-att-widget.js
@@ -45,23 +45,39 @@ angular
         }
       }
       if (vm.period === 'last53weeks') {
-        console.log('53weeks');
+        window.alert();
+        window.alert(JSON.stringify(days , null, 2));
         let week = 0,
             n = days.length - 1;
         data = Array(53).fill(0);
         window.alert(data);
         window.alert(new Date().getTimezoneOffset());
         window.alert(new Date(today.getTime() + (new Date().getTimezoneOffset() + tz*60)*60000).getDay());
+        window.alert('n: '+n);
+        window.alert('week: '+week);
         for (let i = 0; i < 371; i++) {
+          window.alert('i: '+i);
           let day = fzDate.addDays(today, -i);
+          window.alert('day: '+day);
+          window.alert('days[n].date - day === 0: '+ (days[n].date - day === 0));
           if (days[n].date - day === 0) {
+            window.alert('data[52 - week]: '+data[52 - week]);
             data[52 - week] += days[n].att;
+            window.alert('data[52 - week]: '+data[52 - week]);
             n--;
+            window.alert('n: '+n);
           }
+          window.alert('monday?: '+(new Date(day.getTime() + (new Date().getTimezoneOffset() + tz*60)*60000).getDay() === 1));
+          //finish data gathering from "days" on 53rd monday
           if (new Date(day.getTime() + (new Date().getTimezoneOffset() + tz*60)*60000).getDay() === 1) {
+            //labels just for mondays so that they fit in mobile screens
+            window.alert('labels[52 - week]: '+labels[52 - week]);
             labels[52 - week] = (week % 4) ? '' : pad(day.getDate()) +'.'+ pad(day.getMonth() + 1);
+            window.alert('labels[52 - week]: '+labels[52 - week]);
+            window.alert('(week === 52): '+(week === 52));
             if (week === 52) break;
             week++;
+            window.alert('week: '+week);
           }
         }
         window.alert(data);
@@ -85,6 +101,7 @@ angular
       $('#attChartBoxBody').html('').append('<canvas id="attChart" height="240" style="display: block;"></canvas>');
       var ctx = $('#attChart').get(0).getContext('2d');
       var myNewChart = new Chart(ctx).Bar(chartData, {barValueSpacing : 1});
+      window.alert('chart should be redrawed');
     }
 
     function pad(num) { return (num < 10) ? '0' + num : num; }
