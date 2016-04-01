0fe8226d388412f131c8412dcba4c4050849e7de
PS <monsterxxx@yandex.ru>
2016-04-01 07:06:37 +0700

    added month-picker

--- client/components/group/fz-group-filter/fz-group-filter.js
+++ client/components/group/fz-group-filter/fz-group-filter.js
@@ -21,12 +21,17 @@ function Ctrl($scope, $reactive, $stateParams) {
   // console.log('fz-group-filter Ctrl');
   const vm = this,
         companyId = $stateParams.companyId;
+  vm.showPicker = false;
   $reactive(vm).attach($scope);
   vm.helpers({ trainers: () => {
                  let company = Companies.findOne(companyId, {fields: {trainers: 1}});
                  if (company) return company.trainers;
                }
              });
+  console.log($('#month-picker'));
+  $('#month-picker').MonthPicker({ StartYear: 2020, ShowIcon: true });
+  // $('#month-picker').MonthPicker({ StartYear: 2020, ShowIcon: false });
+  // $('#month-picker').MonthPicker({'Position': {my: 'left top', at: 'left bottom', of: '#month-picker-toggle'}});
 }
 
 })();
